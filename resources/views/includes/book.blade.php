bc7628bf70aea565f3a2dad6898592bdf752c78c
Joseph Rooke <jrooke93@gmail.com>
2020-08-01 19:18:36 +0100

    Add catagories to book card

--- resources/views/includes/book.blade.php
+++ resources/views/includes/book.blade.php
@@ -3,8 +3,11 @@
         <img class="card-img-top" src="{{$book->cover_image}}" alt="Card image cap">
     </a>
     <div class="card-body">
-        <h4 class="card-title">{{$book->book_name}}</h4>
-        <h5 class="card-subtitle">by {{$book->author}}</h5>
+        <h4 class="card-title text-dark">{{$book->book_name}}</h4>
+        <h5 class="card-subtitle text-secondary">by {{$book->author}}</h5>
+        @foreach($book->catagories as $catagory)
+        <span class="card-text text-info">{{$catagory->catagory_name}} · </span>
+        @endforeach
     </div>
     <div class="card-footer d-flex justify-content-between">
         <a href="/book/{{$book->id}}" class="btn btn-primary">See details</a>
