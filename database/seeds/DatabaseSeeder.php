4c9136a515d306877ddc97ae70048c2efac58c91
Joseph Rooke <jrooke93@gmail.com>
2020-08-02 14:29:49 +0100

    Seed users table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CatagoriesTableSeeder::class);
         $this->call(BooksTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 2)->create();
+
+        DB::table('users')->insert(
+            array(
+                'name' => 'Admin',
+                'is_admin' => 1,
+                'email' => 'admin@astonbookstore.co.uk',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10)
+            )
+        );
+    }
+}
