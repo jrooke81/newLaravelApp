f9e1996f09b1eff3775a98aa2022ad9555978a1e
Joseph Rooke <jrooke93@gmail.com>
2020-08-03 16:35:44 +0100

    delete basket

--- resources/js/basket.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("select#quantity").change(function(){
-        var selectedQuantity = $(this).children("option:selected").val();
-        alert("You have selected the quantity - " + selectedQuantity);
-        this.form.submit();
-    });
-})
